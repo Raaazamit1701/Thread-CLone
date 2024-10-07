10deef032d49e801326f6be7ebc3159ad5bf042d
Raaazamit1701 <amitraaaz0@gmail.com>
2024-10-07 15:49:41 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,12 @@ cloudinary.config({
 app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
+app.use(cors({
+	origin: process.env.NODE_ENV === "production"
+	  ? "https://thread-clone-ujbh.onrender.com/"  // Replace with your actual frontend URL
+	  : "http://localhost:3000",
+	credentials: true,
+  }));
 
 // Routes
 app.use("/api/users", userRoutes);
